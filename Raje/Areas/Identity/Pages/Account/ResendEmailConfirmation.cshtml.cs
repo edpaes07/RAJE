1d6ff1c5941be6a5e878f2c791f1878abc395745
Edmilson Bispo Paes dos Santos <edmilson.santos@iteris.com.br>
2021-04-21 23:30:07 -0300

    Tela de Login e Cadastro

--- Raje/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ Raje/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
