97e5528b15158dc06166ae26c40d20e34bcc1923
Edmilson Bispo Paes dos Santos <edmilson.santos@iteris.com.br>
2021-04-22 01:35:25 -0300

    Ajustes

--- Raje/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Raje/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -141,7 +141,7 @@ namespace Raje.Areas.Identity.Pages.Account
                             protocol: Request.Scheme);
 
                         await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicando aqui</a>.");
 
                         // If account confirmation is required, we need to show the link if we don't have a real email sender
                         if (_userManager.Options.SignIn.RequireConfirmedAccount)
