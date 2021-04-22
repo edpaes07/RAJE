f9d9858e13a2cc60659ab5f65e5ca8d615cceb66
Edmilson Bispo Paes dos Santos <edmilson.santos@iteris.com.br>
2021-04-21 21:49:17 -0300

    Tela de Cadastro

--- Raje/Controllers/HomeController.cs
+++ Raje/Controllers/HomeController.cs
@@ -102,6 +102,11 @@ namespace Raje.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
