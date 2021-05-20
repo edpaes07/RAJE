aa0a8a22e19065e353fe551acd49bd30852ef531
Edmilson Bispo Paes dos Santos <edmilson.santos@iteris.com.br>
2021-05-20 17:57:36 -0300

    Retirando itens desnecessarios

--- Raje/WC.cs
+++ Raje/WC.cs
@@ -7,13 +7,10 @@ namespace Raje
 {
     public static class WC
     {
-        public const string ImagePath = @"\images\product\";
-        public const string SessionCart = "ShoppingCartSession";
-
         public const string AdminRole = "Admin";
+
         public const string CustomerRole = "Customer";
 
         public const string EmailAdmin = "ben.spark90@yahoo.com";
-
     }
 }
